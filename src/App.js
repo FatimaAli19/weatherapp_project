50ac9323c8706926e28d969eaa07754098406bf7
Anamimdad <144683993+Anamimdad@users.noreply.github.com>
2023-12-18 15:31:51 +0500

    footer implemented

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'; 
 import HeaderSec from './Assets/components/Header';   
+import FooterSec from './Assets/components/Footer';
 
 function App() {
   return (
     <div>
       <HeaderSec/>  
+      <FooterSec/>
     </div>
   );
 }
---
+++ src/Assets/components/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './styles.css';
+
+function FooterSec() {
+  return (
+    <footer className="footer bg-color py-4"> {/* Add py-4 for top and bottom padding */}
+      <div className="container">
+        <div className="row">
+          <div className="col-12 d-flex align-items-center justify-content-center">
+            <h6>&copy; All rights reserved by @WeatherSphere</h6>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default FooterSec;
